1628b15ae09a4cae1064d0decf730128737605ee
Aaron Ortega <aaron@umbrella.media>
2020-05-22 05:09:41 +0000

    mods to js

--- js/sleep-times.js
+++ js/sleep-times.js
@@ -26,109 +26,111 @@ function sleepback(hr, min, an) {
 
 $(document).ready(function() {
   var values = {};
+  let timeinput = 0;
 
   $("#sleep-time-value").on("change", function() {
-    var time = $(this)
-      .val()
-      .split(":");
+    clearTimeout(timeinput);
+    timeinput = setTimeout(function() {
+      var time = $(this)
+        .val()
+        .split(":");
 
-    var gd = new Date();
-    var year = gd.getFullYear();
-    var month = gd.getMonth();
-    var day = gd.getDate();
-    var hr = time[0];
-    var min = time[1];
-    var d = new Date(year, month, day, hr, min);
-    var ampm = "AM";
+      var gd = new Date();
+      var year = gd.getFullYear();
+      var month = gd.getMonth();
+      var day = gd.getDate();
+      var hr = time[0];
+      var min = time[1];
+      var d = new Date(year, month, day, hr, min);
+      var ampm = "AM";
 
-    if (hr > 11) {
-      ampm = "PM";
-    }
-
-    var orig = [hr, min, ampm];
-
-    var first = true;
-    var txt = '<div class="times">';
-
-    var times = new Array();
-    for (var c = 1; c <= 10; c++) {
-      var back = sleepback(hr, min, ampm);
-      var nhr = back[0];
-      var nmin = back[1];
-      ampm = back[2];
-      var ampmt = "";
-      ampmt = back[2];
-
-      // countdown from 12, but that's not
-      // how am/pm system works... whoops!
-      if (nhr == 12) {
-        if (ampm == "AM") {
-          ampmt = "PM";
-        } else {
-          ampmt = "AM";
-        }
+      if (hr > 11) {
+        ampm = "PM";
       }
-      // TODO: reverse display order
-      if (c == 6 || c == 4 || c == 5 || c == 3) {
-        var temp = "";
-        if (nmin > 9) {
-          temp =
-            '<div class="time-bit" rel="' +
-            nhr +
-            ":" +
-            nmin +
-            " " +
-            ampmt +
-            '">' +
-            nhr +
-            ":" +
-            nmin +
-            " " +
-            ampmt +
-            "</div>";
-          times.push(temp);
-        } else {
-          // insert 0
 
-          temp =
-            '<font color="#01DF74" size="7">' +
-            nhr +
-            ":0" +
-            nmin +
-            " " +
-            ampmt +
-            "</font>";
-          temp =
-            '<div class="time-bit" rel="' +
-            nhr +
-            ":0" +
-            nmin +
-            " " +
-            ampmt +
-            '">' +
-            nhr +
-            ":0" +
-            nmin +
-            " " +
-            ampmt +
-            "</div>";
-          times.push(temp);
+      var orig = [hr, min, ampm];
+
+      var first = true;
+      var txt = '<div class="times">';
+
+      var times = new Array();
+      for (var c = 1; c <= 10; c++) {
+        var back = sleepback(hr, min, ampm);
+        var nhr = back[0];
+        var nmin = back[1];
+        ampm = back[2];
+        var ampmt = "";
+        ampmt = back[2];
+
+        // countdown from 12, but that's not
+        // how am/pm system works... whoops!
+        if (nhr == 12) {
+          if (ampm == "AM") {
+            ampmt = "PM";
+          } else {
+            ampmt = "AM";
+          }
         }
+        // TODO: reverse display order
+        if (c == 6 || c == 4 || c == 5 || c == 3) {
+          var temp = "";
+          if (nmin > 9) {
+            temp =
+              '<div class="time-bit" rel="' +
+              nhr +
+              ":" +
+              nmin +
+              " " +
+              ampmt +
+              '">' +
+              nhr +
+              ":" +
+              nmin +
+              " " +
+              ampmt +
+              "</div>";
+            times.push(temp);
+          } else {
+            // insert 0
+
+            temp =
+              '<font color="#01DF74" size="7">' +
+              nhr +
+              ":0" +
+              nmin +
+              " " +
+              ampmt +
+              "</font>";
+            temp =
+              '<div class="time-bit" rel="' +
+              nhr +
+              ":0" +
+              nmin +
+              " " +
+              ampmt +
+              '">' +
+              nhr +
+              ":0" +
+              nmin +
+              " " +
+              ampmt +
+              "</div>";
+            times.push(temp);
+          }
+        }
+        hr = nhr;
+        min = nmin;
       }
-      hr = nhr;
-      min = nmin;
-    }
-    for (i = 3; i >= 0; i--) {
-      txt = txt + times[i];
-    }
+      for (i = 3; i >= 0; i--) {
+        txt = txt + times[i];
+      }
 
-    txt =
-      txt +
-      '<div class="time-bit time-bit-empty"></div><div class="time-bit time-bit-empty"></div></div>';
+      txt =
+        txt +
+        '<div class="time-bit time-bit-empty"></div><div class="time-bit time-bit-empty"></div></div>';
 
-    $("#sleep-times-container").html(txt);
-    $("#sleep-times-container").fadeIn("slow");
-
-    return false;
+      $("#sleep-times-container").html(txt);
+      $("#sleep-times-container").show();
+    }, 1000);
   });
 });
